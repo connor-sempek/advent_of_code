ca531500688de96feb0068d6684524d0e9ef1861
connor-sempek <cpsempek@gmail.com>
2021-12-20 09:45:21 -0800

    add type hints to optional arguments

--- 2021/utils.py
+++ 2021/utils.py
@@ -1,13 +1,17 @@
 # imports
 import os
-from typing import List, Union
+from typing import List, Optional, Union
 
 # Read in puzzle inputs from `data` folder at root of repo.
 # Files containing puzzle inputs are formatted as 2021_day_<day number>_input.txt
 INPUT_DIR = 'data'
 INPUT_FN_TEMPLATE = '2021_day_{}_input.txt'
 
-def get_puzzle_input(day: int, input_dir=INPUT_DIR, input_fn=None) -> List[str]:
+def get_puzzle_input(
+        day: int, 
+        input_dir: str=INPUT_DIR, 
+        input_fn: Optional[str]=None
+    ) -> List[str]:
     """load puzzle input for a given `day` value from the `data` folder at the root of the repo.
     """
     input_fn = input_fn if input_fn is not None else INPUT_FN_TEMPLATE.format(day)
